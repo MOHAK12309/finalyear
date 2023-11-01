e0238b4408154770e8f331ea37afa47d4f99e1d2
Vanshika-026 <vanshikachau26@gmail.com>
2023-11-01 18:29:10 +0530

    dashboard initialize

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import ReactGA from "react-ga4";
 import ReactPixel from "react-facebook-pixel";
 import Home from "./components/home";
 import RenderHome from "./components/renderhome";
-import About from "./components/about";
+import Log from "./components/log";
+import Dashboard from "./components/dash-one";
 // importing service creation pages
 
 // ReactPixel.init('390972365769622',  options);
@@ -19,7 +20,8 @@ function App() {
       <div className="App"> 
         <Routes>
           <Route path="/renderhome" element={<RenderHome />}></Route>
-          <Route path="/about" element={<About />}></Route>
+          <Route path="/log" element={<Log />}></Route>
+          <Route path="/dashboard" element={<Dashboard/>}></Route>
         </Routes>
       </div>
     </div>
