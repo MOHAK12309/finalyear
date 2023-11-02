f2a8d0063e049645081e3b1509345a1d608c5287
vaishu-15 <vaishali15yadav@gmail.com>
2023-11-03 01:04:18 +0530

    company reg

--- src/components/Creg.js
+++ src/components/Creg.js
@@ -1,56 +1,98 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Creg.css";
-//const illus = new URL("../images/image1.jpg", import.meta.url);
 
 function Reg() {
   return (
     <>
-      <div className="wrapper">
-        <form action="">
-          <h1>Registration</h1>
-          <div className="input-box">
-            <div className="input-field">
-              <input type="text" placeholder="Name" required></input>
+      <div class="login-wrap">
+        <div class="login-html">
+          <input id="tab-1" type="radio" name="tab" class="sign-in" checked />
+          <label for="tab-1" class="tab">
+            Sign In
+          </label>
+          <input id="tab-2" type="radio" name="tab" class="sign-up" />
+          <label for="tab-2" class="tab">
+            Sign Up
+          </label>
+          <div className="login-form">
+            <div className="sign-in-htm">
+              <div className="group">
+                <label for="user" className="label">
+                  Username
+                </label>
+                <input id="user" type="text" className="input" />
+              </div>
+              <div className="group">
+                <label for="pass" className="label">
+                  Password
+                </label>
+                <input
+                  id="pass"
+                  type="password"
+                  className="input"
+                  data-type="password"
+                />
+              </div>
+              <div className="group">
+                <input id="check" type="checkbox" className="check" checked />
+                <label for="check">
+                  <span className="icon"></span> Keep me Signed in
+                </label>
+              </div>
+              <div className="group">
+                <input type="submit" className="button" value="Sign In" />
+              </div>
+              <div className="hr"></div>
+              <div className="foot-lnk">
+                <a href="#forgot">Forgot Password?</a>
+              </div>
+            </div>
+            <div className="sign-up-htm">
+              <div className="group">
+                <label for="user" class="label">
+                  Username
+                </label>
+                <input id="user" type="text" className="input" />
+              </div>
+              <div className="group">
+                <label for="pass" class="label">
+                  Password
+                </label>
+                <input
+                  id="pass"
+                  type="password"
+                  className="input"
+                  data-type="password"
+                />
+              </div>
+              <div className="group">
+                <label for="pass" className="label">
+                  Repeat Password
+                </label>
+                <input
+                  id="pass"
+                  type="password"
+                  className="input"
+                  data-type="password"
+                />
+              </div>
+              <div className="group">
+                <label for="pass" class="label">
+                  Email Address
+                </label>
+                <input id="pass" type="text" className="input" />
+              </div>
+              <div className="group">
+                <input type="submit" className="button" value="Sign Up" />
+              </div>
+              <div className="hr"></div>
+              <div className="foot-lnk">
+                <label for="tab-1">Already Member?</label>
+              </div>
             </div>
           </div>
-          <div className="input-box">
-            <div className="input-field">
-              <input type="text" placeholder="Company Name" required></input>
-            </div>
-          </div>
-          <div class="dropdown">
-            <button
-              class="btn btn-secondary dropdown-toggle"
-              type="button"
-              id="dropdownMenu2"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Company Type
-            </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-              <button class="dropdown-item" type="button">
-                Action
-              </button>
-              <button class="dropdown-item" type="button">
-                Another action
-              </button>
-              <button class="dropdown-item" type="button">
-                Something else here
-              </button>
-            </div>
-          </div>
-          <div className="input-box">
-            <div className="input-field">
-              <input
-                type="Number"
-                placeholder="Phone Number (Optional)"
-              ></input>
-            </div>
-          </div>
-        </form>
+        </div>
       </div>
     </>
   );
