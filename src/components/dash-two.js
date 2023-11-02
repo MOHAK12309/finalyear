2636c2bdbec07c397dfeaee2dcb4dbcfee452c16
vaishu-15 <vaishali15yadav@gmail.com>
2023-11-02 22:52:12 +0530

    dash

---
+++ src/components/dash-two.js
@@ -0,0 +1,316 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/dash-two.css";
+const illus = new URL("../images/image1.jpg", import.meta.url);
+
+function Dashtwo() {
+  return (
+    <>
+      <div class="container">
+        <div class="navigation">
+            <ul>
+                <li>
+                    <a href="#">
+                        <span className="icon">
+                            <ion-icon name="logo-apple"></ion-icon>
+                        </span>
+                        <span className="title">Brand Name</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <span className="icon">
+                            <ion-icon name="home-outline"></ion-icon>
+                        </span>
+                        <span className="title">Dashboard</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <span className="icon">
+                            <ion-icon name="people-outline"></ion-icon>
+                        </span>
+                        <span className="title">Customers</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <span className="icon">
+                            <ion-icon name="chatbubble-outline"></ion-icon>
+                        </span>
+                        <span className="title">Messages</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <span className="icon">
+                            <ion-icon name="help-outline"></ion-icon>
+                        </span>
+                        <span className="title">Help</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <span className="icon">
+                            <ion-icon name="settings-outline"></ion-icon>
+                        </span>
+                        <span className="title">Settings</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <span className="icon">
+                            <ion-icon name="lock-closed-outline"></ion-icon>
+                        </span>
+                        <span className="title">Password</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <span className="icon">
+                            <ion-icon name="log-out-outline"></ion-icon>
+                        </span>
+                        <span className="title">Sign Out</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div className="main">
+            <div className="topbar">
+                <div className="toggle">
+                    <ion-icon name="menu-outline"></ion-icon>
+                </div>
+
+                <div className="search">
+                    <label>
+                        <input type="text" placeholder="Search here"/>
+                        <ion-icon name="search-outline"></ion-icon>
+                    </label>
+                </div>
+
+                <div className="user">
+                    <img src="assets/imgs/customer01.jpg" alt=""></img>
+                </div>
+            </div>
+
+
+            <div className="cardBox">
+                <div className="card">
+                    <div>
+                        <div className="numbers">1,504</div>
+                        <div className="cardName">Daily Views</div>
+                    </div>
+
+                    <div className="iconBx">
+                        <ion-icon name="eye-outline"></ion-icon>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <div>
+                        <div className="numbers">80</div>
+                        <div className="cardName">Sales</div>
+                    </div>
+
+                    <div className="iconBx">
+                        <ion-icon name="cart-outline"></ion-icon>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <div>
+                        <div className="numbers">284</div>
+                        <div className="cardName">Comments</div>
+                    </div>
+
+                    <div className="iconBx">
+                        <ion-icon name="chatbubbles-outline"></ion-icon>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <div>
+                        <div class="numbers">$7,842</div>
+                        <div class="cardName">Earning</div>
+                    </div>
+
+                    <div className="iconBx">
+                        <ion-icon name="cash-outline"></ion-icon>
+                    </div>
+                </div>
+            </div>
+
+            
+            <div className="details">
+                <div className="recentOrders">
+                    <div className="cardHeader">
+                        <h2>Recent Orders</h2>
+                        <a href="#" class="btn">View All</a>
+                    </div>
+
+                    <table>
+                        <thead>
+                            <tr>
+                                <td>Name</td>
+                                <td>Price</td>
+                                <td>Payment</td>
+                                <td>Status</td>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                                <td>Star Refrigerator</td>
+                                <td>$1200</td>
+                                <td>Paid</td>
+                                <td><span class="status delivered">Delivered</span></td>
+                            </tr>
+
+                            <tr>
+                                <td>Dell Laptop</td>
+                                <td>$110</td>
+                                <td>Due</td>
+                                <td><span class="status pending">Pending</span></td>
+                            </tr>
+
+                            <tr>
+                                <td>Apple Watch</td>
+                                <td>$1200</td>
+                                <td>Paid</td>
+                                <td><span class="status return">Return</span></td>
+                            </tr>
+
+                            <tr>
+                                <td>Addidas Shoes</td>
+                                <td>$620</td>
+                                <td>Due</td>
+                                <td><span class="status inProgress">In Progress</span></td>
+                            </tr>
+
+                            <tr>
+                                <td>Star Refrigerator</td>
+                                <td>$1200</td>
+                                <td>Paid</td>
+                                <td><span class="status delivered">Delivered</span></td>
+                            </tr>
+
+                            <tr>
+                                <td>Dell Laptop</td>
+                                <td>$110</td>
+                                <td>Due</td>
+                                <td><span class="status pending">Pending</span></td>
+                            </tr>
+
+                            <tr>
+                                <td>Apple Watch</td>
+                                <td>$1200</td>
+                                <td>Paid</td>
+                                <td><span class="status return">Return</span></td>
+                            </tr>
+
+                            <tr>
+                                <td>Addidas Shoes</td>
+                                <td>$620</td>
+                                <td>Due</td>
+                                <td><span class="status inProgress">In Progress</span></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                
+                <div className="recentCustomers">
+                    <div className="cardHeader">
+                        <h2>Recent Customers</h2>
+                    </div>
+
+                    <table>
+                        <tr>
+                            <td width="60px">
+                                <div class="imgBx"><img src="assets/imgs/customer02.jpg" alt=""/></div>
+                            </td>
+                            <td>
+                                <h4>David <br></br> <span>Italy</span></h4>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td width="60px">
+                                <div class="imgBx"><img src="assets/imgs/customer01.jpg" alt=""></img></div>
+                            </td>
+                            <td>
+                                <h4>Amit <br></br> <span>India</span></h4>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td width="60px">
+                                <div class="imgBx"><img src="assets/imgs/customer02.jpg" alt=""></img></div>
+                            </td>
+                            <td>
+                                <h4>David <br></br> <span>Italy</span></h4>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td width="60px">
+                                <div class="imgBx"><img src="assets/imgs/customer01.jpg" alt=""></img></div>
+                            </td>
+                            <td>
+                                <h4>Amit <br></br> <span>India</span></h4>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td width="60px">
+                                <div class="imgBx"><img src="assets/imgs/customer02.jpg" alt=""></img></div>
+                            </td>
+                            <td>
+                                <h4>David <br></br> <span>Italy</span></h4>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td width="60px">
+                                <div class="imgBx"><img src="assets/imgs/customer01.jpg" alt=""></img></div>
+                            </td>
+                            <td>
+                                <h4>Amit <br></br> <span>India</span></h4>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td width="60px">
+                                <div class="imgBx"><img src="assets/imgs/customer01.jpg" alt=""></img></div>
+                            </td>
+                            <td>
+                                <h4>David <br></br> <span>Italy</span></h4>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td width="60px">
+                                <div class="imgBx"><img src="assets/imgs/customer02.jpg" alt=""></img></div>
+                            </td>
+                            <td>
+                                <h4>Amit <br></br> <span>India</span></h4>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </>
+  );
+    }
+export default Dashtwo
