92b8fbcd8163031eeff0d6293fd5d06d60329916
vaishu-15 <vaishali15yadav@gmail.com>
2023-12-01 15:04:27 +0530

    dashboard changes

--- src/components/dash-two.js
+++ src/components/dash-two.js
@@ -7,7 +7,8 @@ import Drawer from "react-modern-drawer";
 import "../styles/OTP.css";
 import "react-modern-drawer/dist/index.css";
 import Dashboard from "./dash-one";
-const logo = new URL("../images/logo.png", import.meta.url);
+const logo = new URL("../images/logo-no-background.png", import.meta.url);
+const logo2 = new URL("../images/hamburger.png", import.meta.url);
 const App2 = () => {
   const [side, setSide] = useState(false);
   const toggleMode = () => {
@@ -31,7 +32,7 @@ const App2 = () => {
                   <img
                     src={logo}
                     alt="Logo"
-                    style={{ width: "70px", height: "45px" }}
+                    style={{ width: "100px", height: "35px" }}
                   />
                 </h2>
               </div>
@@ -54,18 +55,18 @@ const App2 = () => {
         </div>
         {/*right   */}
         <div style={{ width: "100%" }}>
-          <div className="nav2" style={{ padding: "13px" }}>
-            <div>
-              <h4 onClick={toggleMode} style={{ cursor: "pointer" }}>
-                hamburger
-              </h4>
-            </div>
-            <div>
-              <button className="logout">Logout</button>
-            </div>
-          </div>
           {/* dahsboard */}
-          <div style={{ maxHeight: "92vh", overflowY: "scroll" }}>
+          <div className="dashboardmain">
+            <div className="nav2" style={{ padding: "13px" }}>
+              <div>
+                <h4 onClick={toggleMode} style={{ cursor: "pointer" }}>
+                  <img width="30px" src={logo2}></img>
+                </h4>
+              </div>
+              <div>
+                <button className="logout">Logout</button>
+              </div>
+            </div>
             <Dashboard />
           </div>
         </div>
