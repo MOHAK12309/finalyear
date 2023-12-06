cfa5460b81a54a62005381435bab7c01ef0f3243
MOHAK12309 <yashraj63951@gmail.com>
2023-12-06 18:12:22 +0530

    mapdone

--- src/components/dash-two.js
+++ src/components/dash-two.js
@@ -1,19 +1,36 @@
-import React from "react";
-import { useState } from "react";
-// import component 👇
+import React, { useState, useEffect } from "react";
 import Drawer from "react-modern-drawer";
-
-//import styles 👇
-import "../styles/OTP.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import axios from "axios";
+import "leaflet/dist/leaflet.css";
 import "react-modern-drawer/dist/index.css";
+import logo from "../images/logo-no-background.png";
+import logo2 from "../images/hamburger.png";
 import Dashboard from "./dash-one";
-const logo = new URL("../images/logo-no-background.png", import.meta.url);
-const logo2 = new URL("../images/hamburger.png", import.meta.url);
+
 const App2 = () => {
   const [side, setSide] = useState(false);
+  const [visitors, setVisitors] = useState([]);
+  const illus = new URL("../images/image2.jpg", import.meta.url);
+
   const toggleMode = () => {
     setSide(!side);
   };
+
+  useEffect(() => {
+    // Fetch visitor data from the API
+    const fetchVisitorData = async () => {
+      try {
+        const response = await axios.get("https://metainsights.onrender.com/api/v1/visitors");
+        setVisitors(response.data.visitors.value);
+      } catch (error) {
+        console.error("Error fetching visitor data:", error);
+      }
+    };
+
+    fetchVisitorData();
+  }, []); // Run this effect only once, on component mount
+
   return (
     <>
       <div style={{ display: "flex" }}>
@@ -47,32 +64,62 @@ const App2 = () => {
               </button>
               <button className="button-dashboard">
                 <span className={` ${side ? "sidesec2" : ""}`}>
-                  Real time Analysis
+                  Real-time Analysis
                 </span>
               </button>
             </div>
           </div>
         </div>
-        {/*right   */}
+        {/* right */}
         <div style={{ width: "100%" }}>
-         
-          {/* dahsboard */}
+          {/* dashboard */}
           <div className="dashboardmain">
-          
-          <div className="nav2" style={{ padding: "13px" }}>
-      <div className="left-section">
-        <h4 onClick={toggleMode} style={{ cursor: "pointer" }}><img src={logo2} width="30px"></img> </h4>
-        <button className="user-button">User</button>
-        <button className="settings-button">Settings</button>
-      </div>
-      
-      <div className="right-section">
-        <button className="notifications">Notifications</button>
-        <button className="email">Email</button>
-        <button className="logout">Logout</button>
-      </div>
-    </div>
+            <div className="nav2" style={{ padding: "13px" }}>
+              <div className="left-section">
+                <h4 onClick={toggleMode} style={{ cursor: "pointer" }}>
+                  <img src={logo2} width="30px" alt="Hamburger Menu" />
+                </h4>
+                <button className="user-button">User</button>
+                <button className="settings-button">Settings</button>
+              </div>
+
+              <div className="right-section">
+                <button className="notifications">Notifications</button>
+                <button className="email">Email</button>
+                <button className="logout">Logout</button>
+              </div>
+            </div>
             <Dashboard />
+            <div style={{ width: "100%", height: "400px" }}>
+              <MapContainer
+                center={[0, 0]}
+                zoom={2}
+                style={{ width: "100%", height: "100%" }}
+              >
+                <TileLayer
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {visitors.map((visitor) => (
+                  <Marker
+                    key={visitor._id}
+                    position={[
+                      visitor.metaData.location.latitude || 0,
+                      visitor.metaData.location.longitude || 0
+                    ]}
+                    
+                  >
+                    <Popup>
+                      <div>
+                        {/* <h3>{visitor.sID}</h3> */}
+                        <p>Browser: {visitor.metaData.browserName}</p>
+                        <p>Mobile: {visitor.metaData.isMobile.toString()}</p>
+                        <p>Timestamp: {visitor.timestamp}</p>
+                      </div>
+                    </Popup>
+                  </Marker>
+                ))}
+              </MapContainer>
+            </div>
           </div>
         </div>
       </div>
@@ -81,3 +128,9 @@ const App2 = () => {
 };
 
 export default App2;
+
+
+
+
+// 
+
